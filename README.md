a8e930ca7e95d05f022db5cc29b992ffcbfd1687
Erikson <chris.s.erickson@gmail.com>
2022-06-21 19:05:48 -0400

    added questions and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Password Generator Starter Code
+
+## Acceptance Criteria
+
+* Needed Prompts
+    ** password criteria
+    ** lenght of password
+    ** confirm whether or not to include lowercase, upppercase, numeric, and/or special characters - separate prompts for each
+    ** validated with at least 1 of above chosen
+* Password should be displayed in alert or on the page
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
 // Assignment code here
+// how do i ceate a string of variable with numbers, characters, and lower/upper case letters?
 
 
 // Get references to the #generate element
+// Variable created to create the password to display?
+// code from HTML  -- button id="generate" class="btn">Generate Password</button --
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -14,4 +19,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
