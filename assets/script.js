68fb26954e3da65c954ff8ead09da2d1fc029b4e
Erikson <chris.s.erickson@gmail.com>
2022-06-23 19:42:18 -0400

    working on generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,7 @@
 // Assignment code here
-// how do i ceate a string of variable with numbers, characters, and lower/upper case letters?
 
 
 // Get references to the #generate element
-// Variable created to create the password to display?
-// code from HTML  -- button id="generate" class="btn">Generate Password</button --
 // generat password function to generate
 
 var generateBtn = document.querySelector("#generate");
@@ -21,52 +18,75 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var numbers = ["1","2","3","4","5","6","7","8","9","0"]
   console.log(numbers.length + " -- " + numbers)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();{
-    // addressing length of password
-    var length = window.prompt("How many characters would you like your password to contain?")
-    console.log(length);
+function generatePassword() {
+//   do password generating things
+
+  var passwordArray= []
+  var generatedPassword = ""
+  
+  var generatePassword = "This is the text for a generated password"
+    var length = parseInt(window.prompt("How many characters would you like your password to contain?"))
+    console.log("User selected " + length);
     if (length < 8 || length > 128) {
       alert("Password must be between 8-128 characters!")
-      return generatePassword();
+      generatePassword();
     }
+
     var symbolsConfirm = window.confirm("Would you like to use symbols?")
     if (symbolsConfirm){
       // add symbols into the combined array
-    }else{
-
+      passwordArray = passwordArray.concat(symbols)
     }
 
     var upperConfirm = window.confirm("Would you like to use uppercase letters?")
     if (upperConfirm){
       // add upper case into the combined array
-    }else{
-
+      passwordArray=passwordArray.concat(upperCase)
     }
 
     var lowerConfirm =window.confirm("Would you like to use lowercase letters?")
     if (lowerConfirm){
       // add lower case into the combined array
-    }else{
-
+      passwordArray = passwordArray.concat(lowerCase)
     }
 
     var numbersConfirm = window.confirm("Would you like to use numbers?")
     if (numbersConfirm){
       // add numbers into the combined array
-    }else{
-
+      passwordArray = passwordArray.concat(numbers)
     }
-  }
+    
+    if (passwordArray.length === 0) {
+      alert("You must enter at least one value.")
+      generatePassword();
+    }
+
+    // generate a password from the user parameters above
+    for (var i = 0; i < length; i++) {
+      // code to generate password character by character
+      // use a random number Math.floor(Math.random() * passwordArray.length) and
+      return Math.floor(Math.random() * passwordArray)
+      
+      // add the character at that number to the generatedPassword
+      // generatedPassword += passwordArray[random number]
+      
+    }
+
+  // return  generatedPassword
+}
+
+generatePassword();
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  generateBtn.addEventListener("click", password);
 }
 
-// Add event listener to generate button
 
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
